08d7751fb219756fcaef99a9ade484a6552a0f72
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-09 23:30:53 -0500

    app is broken

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 class App extends Component { 
   render() {
     return(
       <div>
-        <h1>My Boiler Plate</h1>
+        <Header />
       </div> 
     )
   }
